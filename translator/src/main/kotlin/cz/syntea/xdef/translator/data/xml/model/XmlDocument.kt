70ec7410817f356053c222028dc01a5ec8a19502
Filip Šmíd <smidfil3@fit.cvut.cz>
2019-04-08 16:02:27 +0200

    Change equals and hashCode implementation

--- translator/src/main/kotlin/cz/syntea/xdef/translator/data/xml/model/XmlDocument.kt
+++ translator/src/main/kotlin/cz/syntea/xdef/translator/data/xml/model/XmlDocument.kt
@@ -18,15 +18,15 @@ data class XmlDocument constructor(
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is cz.syntea.xdef.model.Document) return false
+        if (!super.equals(other)) return false
 
-        if (type != other.type) return false
         if (root != other.root) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = type.hashCode()
+        var result = super.hashCode()
         result = 31 * result + root.hashCode()
         return result
     }
