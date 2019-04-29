89bfceecda1427c5a64368afe81ea1a0786a7964
Filip Šmíd <smidfil3@fit.cvut.cz>
2019-04-29 19:56:25 +0200

    Extension function for stack container

---
+++ translator/src/main/kotlin/cz/syntea/xdef/translator/Extensions.kt
@@ -0,0 +1,13 @@
+package cz.syntea.xdef.translator
+
+import java.util.*
+
+/**
+ * File contains extension functions
+ *
+ * @author [Filip Šmíd](mailto:smidfil3@fit.cvut.cz)
+ */
+
+fun <T> Stack<T>.peekOrNull(): T? {
+    return if (isEmpty()) null else peek()
+}
