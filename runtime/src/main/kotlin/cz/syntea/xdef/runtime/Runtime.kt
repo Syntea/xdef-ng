d38e03757c0aeec970522d7348581e24320bd4db
Filip Šmíd <smidfil3@fit.cvut.cz>
2019-05-01 14:26:58 +0200

    Add Runtime component interface

---
+++ runtime/src/main/kotlin/cz/syntea/xdef/runtime/Runtime.kt
@@ -0,0 +1,54 @@
+package cz.syntea.xdef.runtime
+
+import cz.syntea.xdef.core.definition.XDefinition
+import cz.syntea.xdef.core.document.data.XTree
+import cz.syntea.xdef.core.document.stream.XReader
+import cz.syntea.xdef.core.document.stream.XWriter
+
+/**
+ * TODO CLASS_DESCRIPTION
+ *
+ * @author [Filip Šmíd](mailto:smidfil3@fit.cvut.cz)
+ */
+interface Runtime {
+
+    /**
+     * @param definition
+     * @param input
+     * @param output
+     * @param reporter
+     *
+     * @return
+     */
+    fun parse(definition: XDefinition, input: XTree, output: XWriter?, reporter: Appendable): XTree
+
+    /**
+     * @param definition
+     * @param input
+     * @param output
+     * @param reporter
+     *
+     * @return
+     */
+    fun parse(definition: XDefinition, input: XReader, output: XWriter?, reporter: Appendable): XTree
+
+    /**
+     * @param definition
+     * @param input
+     * @param output
+     * @param reporter
+     *
+     * @return
+     */
+    fun create(definition: XDefinition, input: XTree, output: XWriter?, reporter: Appendable): XTree
+
+    /**
+     * @param definition
+     * @param input
+     * @param output
+     * @param reporter
+     *
+     * @return
+     */
+    fun create(definition: XDefinition, input: XReader, output: XWriter?, reporter: Appendable): XTree
+}
