1022dc931e68ea4cc7ad5b0ec71baa9c8d58b046
Filip Šmíd <smidfil3@fit.cvut.cz>
2019-05-01 14:32:55 +0200

    Add XScriptInfo to X-definition structure

--- core/src/main/kotlin/cz/syntea/xdef/core/definition/XDefinition.kt
+++ core/src/main/kotlin/cz/syntea/xdef/core/definition/XDefinition.kt
@@ -1,5 +1,6 @@
 package cz.syntea.xdef.core.definition
 
+import cz.syntea.xdef.core.XScriptInfo
 import java.io.Serializable
 
 /**
@@ -8,7 +9,14 @@ import java.io.Serializable
  * @author [Filip Šmíd](mailto:smidfil3@fit.cvut.cz)
  */
 data class XDefinition(
-//    val scriptInfo: XScriptInfo,
-//    val factory: XTreeFactory,
+
+    /**
+     * Information about XScript which was used by compilation
+     */
+    val scriptInfo: XScriptInfo,
+
+    /**
+     * Root of tree which represents compiled X-definition
+     */
     val root: XDefinitionTree
 ) : Serializable
