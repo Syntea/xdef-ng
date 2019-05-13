3d055e4e5249a7f8737c0a9e8e897c3da17166b1
Filip Šmíd <smidfil3@fit.cvut.cz>
2019-05-13 13:37:35 +0200

    Refactoring: Move extension function to utils file

--- translator/src/main/kotlin/org/xdef/translator/Utils.kt
+++ translator/src/main/kotlin/org/xdef/translator/Utils.kt
@@ -3,12 +3,17 @@ package org.xdef.translator
 import org.jdom2.Content
 import org.jdom2.located.Located
 import org.xdef.core.Location
+import java.util.*
 
 /**
  * File contains extension and util functions
  *
  * @author [Filip Šmíd](mailto:smidfil3@fit.cvut.cz)
  */
+
+fun <T> Stack<T>.peekOrNull(): T? {
+    return if (isEmpty()) null else peek()
+}
 
 val WS_REGEX = "\\s".toRegex()
 
